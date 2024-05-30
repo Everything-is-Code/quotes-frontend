12e1de1b1c3afcafd298d4b5e46c4796a10f00b7
nacho <ilago@redhat.com>
2024-05-30 18:39:25 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from 'react';
+import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
+    const [count, setCount] = useState(0)
     const [quote, setQuote] = useState('');
 
     useEffect(() => {
         fetch('https://quotes-backend-quotes.apps.cluster-lz7k2.dynamic.redhatworkshops.io/hello/greeting/quote', {
             headers: {
-            'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+                'Access-Control-Allow-Headers': 'Content-Type',
             },
-            mode: 'cors',
         })
             .then(response => response.text())
             .then(data => setQuote(data))
@@ -17,10 +20,20 @@ function App() {
     }, []);
 
     return (
-        <div>
-            <h1>Random Quote:</h1>
-            <p>{quote}</p>
-        </div>
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Random Quote:</h1>
+        <p>{quote}</p>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+      </div>
+     </>
     );
 }
 
